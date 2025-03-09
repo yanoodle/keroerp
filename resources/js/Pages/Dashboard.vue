9b0b709740c25cd48955a847641a6e5f1d79c810
Akbar Sopyan <yansecontact@gmail.com>
2025-03-09 20:28:06 +0700

    second commit

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        You're logged in!
+                        Welcome, You're logged in!
                     </div>
                 </div>
             </div>
