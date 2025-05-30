2151f97093db728b0e574d6b5a8c192918864524
Akbar Sopyan <yansecontact@gmail.com>
2025-05-30 22:18:58 +0700

    bug fix logged in as ...

--- resources/js/Pages/Purchase.vue
+++ resources/js/Pages/Purchase.vue
@@ -238,7 +238,10 @@ const vendorInfoMap = computed(() => {
         >
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
+                    <div
+                        class="p-6 text-gray-900"
+                        v-if="$page.props.auth.user.role === 'Purchase'"
+                    >
                         Welcome, You're logged in as Purchasing
                     </div>
                     <div
--- resources/js/Pages/Sales.vue
+++ resources/js/Pages/Sales.vue
@@ -227,7 +227,10 @@ const statusColors = {
         >
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
+                    <div
+                        class="p-6 text-gray-900"
+                        v-if="$page.props.auth.user.role === 'Sales'"
+                    >
                         Welcome, You're logged in as Sales
                     </div>
                     <div class="p-6 text-gray-900">
